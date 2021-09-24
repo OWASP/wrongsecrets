662a9543d9f078727f23285005786d909e0092ca
Nanne Baars <nbaars@xebia.com>
2021-09-24 11:05:22 +0200

    Add profile to run locally without Spring Vault enabled

---
+++ src/test/java/com/example/secrettextprinter/SecretLeakageControllerTest.java
@@ -0,0 +1,12 @@
+package com.example.secrettextprinter;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class SecretLeakageControllerTest {
+
+
+
+}
