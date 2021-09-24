d8b646725d86da6b7e3b2e680e8ee09049ec3a2e
Nanne Baars <nbaars@xebia.com>
2021-09-24 11:53:00 +0200

    Add test case for first challenge

--- src/test/java/com/example/secrettextprinter/SecretLeakageControllerTest.java
+++ src/test/java/com/example/secrettextprinter/SecretLeakageControllerTest.java
@@ -1,12 +1,54 @@
 package com.example.secrettextprinter;
 
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith({SpringExtension.class})
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
 class SecretLeakageControllerTest {
 
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
+    @Value("${password}")
+    private String hardcodedPassword;
 
+
+    @BeforeEach
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
+    @Test
+    void spoil1() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/spoil-1"))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(CoreMatchers.containsString(hardcodedPassword)));
+    }
+
+    @Test
+    void solveChallenge1() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/challenge/1").param("solution", hardcodedPassword))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(CoreMatchers.containsString("You&#39;re answer is correct!")));
+    }
 
 }
