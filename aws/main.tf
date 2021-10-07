ace9bc1ac783ca11554d87748fe8b3c92c9d9783
Jeroen Willemsen <jwillemsen@xebia.com>
2021-10-07 17:23:15 +0200

    Adding scripts for patching Fargate worker nodes

--- aws/main.tf
+++ aws/main.tf
@@ -101,6 +101,32 @@ module "eks" {
   ]
 }
 
+######
+# PAtching as per https://github.com/terraform-aws-modules/terraform-aws-eks/issues/1286#issuecomment-811157662
+######
+# Provider configuration
+provider "shell" {
+  interpreter = ["/bin/bash", "-c"]
+  sensitive_environment = {
+    KUBECTL_CONFIG = base64encode(module.eks.kubeconfig)
+  }
+}
+
+# Configures coredns to run on Fargate.
+# Per default coredns runs with EC2. 
+# The Terraform eks module does not offer any inputs to set the compute type of coredns to Fargate.
+# See: https://github.com/terraform-aws-modules/terraform-aws-eks/issues/1286
+# Therefore, we are using the kubectl to patch coredns using the Kubernetes API.
+resource "shell_script" "coredns_fargate_patch" {
+  lifecycle_commands {
+    create = file("${path.module}/scripts/patch_coredns_for_fargate.sh")
+    delete = file("${path.module}/scripts/patch_coredns_for_ec2.sh")
+  }
+
+  # Wait for the EKS module to get provisioned completely including the kube-system Fargate profile.
+  depends_on = [module.eks]
+}
+
 
 
 #############
--- aws/vesions.tf
+++ aws/vesions.tf
@@ -3,9 +3,13 @@ terraform {
   required_version = ">= 0.13.1"
 
   required_providers {
-    aws        = ">= 3.22.0"
-    local      = ">= 1.4"
-    random     = ">= 2.1"
-    kubernetes = ">= 1.11"
+    aws                 = ">= 3.22.0"
+    local               = ">= 1.4"
+    random              = ">= 2.1"
+    kubernetes          = ">= 1.11"
+    shell = {
+      source = "scottwinkler/shell"
+      version = "1.7.7"
+    }
   }
 }
