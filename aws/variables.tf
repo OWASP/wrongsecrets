088c367bb80321b408bb04f4b63a7feafa090112
Jeroen Willemsen <jwillemsen@xebia.com>
2021-10-26 22:19:13 +0200

    small fixes to make vault iss claims work

--- aws/variables.tf
+++ aws/variables.tf
@@ -7,7 +7,7 @@ variable "region" {
 variable "cluster_version" {
   description = "The EKS cluster version to use"
   type        = string
-  default     = "1.21"
+  default     = "1.20.7"
 }
 
 variable "cluster_name" {
