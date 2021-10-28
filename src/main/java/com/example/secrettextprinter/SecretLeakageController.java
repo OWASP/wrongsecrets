4c3fade7f4350ca76674dffdf8805c59a8d3eb7c
Jeroen Willemsen <jwillemsen@xebia.com>
2021-10-28 08:47:52 +0200

    Added better warnings

--- src/main/java/com/example/secrettextprinter/SecretLeakageController.java
+++ src/main/java/com/example/secrettextprinter/SecretLeakageController.java
@@ -135,10 +135,18 @@ public class SecretLeakageController {
     public String challengeForm(@PathVariable String id, Model model) {
         model.addAttribute("challengeForm", new ChallengeForm());
         model.addAttribute("challengeNumber", id);
-        if (!isDockerized()) {
-            model.addAttribute("dockerizedWarning", "We are running outside of a docker container. Please run this in a container.");
+        if (!isDockerized() && (Integer.parseInt(id) < 5 || "8".equals(id))) {
+            model.addAttribute("runtimeWarning", "We are running outside of a docker container. Please run this in a container as explained in the README.md.");
         }
-
+        if (("5".equals(id) || "6".equals(id)) && "if_you_see_this_please_use_k8s".equals(configmapK8sSecret)) {
+            model.addAttribute("runtimeWarning", "We are running outside of a K8s cluster. Please run this in the K8s cluster as explained in the README.md.");
+        }
+        if ("7".equals(id) && vaultPassword.getPasssword() == null) {
+            model.addAttribute("runtimeWarning", "We are running outside of a K8s cluster with Vault. Please run this in the K8s cluster as explained in the README.md.");
+        }
+        if (("9".equals(id) || "10".equals(id) || "11".equals(id)) && "if_you_see_this_please_use_AWS_Setup".equals(awsRoleArn)) {
+            model.addAttribute("runtimeWarning", "We are running outside of a properly configured AWS environment. Please run this in an AWS environment as explained in the README.md.");
+        }
         return "challenge";
     }
 
