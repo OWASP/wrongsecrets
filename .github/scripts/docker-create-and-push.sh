47281203fdd2701c851bd1aad9a5c0f3edd45145
Jeroen Willemsen <jwillemsen@xebia.com>
2021-11-01 06:09:40 +0100

    Release 1.0.0

--- .github/scripts/docker-create-and-push.sh
+++ .github/scripts/docker-create-and-push.sh
@@ -11,8 +11,8 @@ echo "tag message: $2"
 echo "buildarg supplied: $3"
 echo "Port: $4"
 
-#git tag -a $1 -m "$2"
-#git push --tags
+git tag -a $1 -m "$2"
+git push --tags
 docker build --build-arg "$3" --build-arg "PORT=8081" --build-arg "argBasedVersion=$1" --build-arg "spring_profile=without-vault" -t jeroenwillemsen/addo-example:$1-no-vault ./../../.
 docker push jeroenwillemsen/addo-example:$1-no-vault
 docker build --build-arg "$3" --build-arg "PORT=8081" --build-arg "argBasedVersion=$1" --build-arg "spring_profile=local-vault" -t jeroenwillemsen/addo-example:$1-local-vault ./../../.
