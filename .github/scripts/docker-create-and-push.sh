c0853672b0e5715af293ceb9e5ad0c2d567d0a55
Jeroen Willemsen <jwillemsen@xebia.com>
2021-11-25 23:01:25 +0100

    Added new container (wrongsecrets) https://github.com/commjoen/wrongsecrets/issues/74

--- .github/scripts/docker-create-and-push.sh
+++ .github/scripts/docker-create-and-push.sh
@@ -19,6 +19,10 @@ echo "creating containers"
 docker buildx build --platform linux/amd64,linux/arm64 -t jeroenwillemsen/addo-example:$1-no-vault --build-arg "$3" --build-arg "PORT=8081" --build-arg "argBasedVersion=$1" --build-arg "spring_profile=without-vault" --push ./../../.
 docker buildx build --platform linux/amd64,linux/arm64 -t jeroenwillemsen/addo-example:$1-local-vault --build-arg "$3" --build-arg "PORT=8081" --build-arg "argBasedVersion=$1" --build-arg "spring_profile=local-vault" --push ./../../.
 docker buildx build --platform linux/amd64,linux/arm64 -t jeroenwillemsen/addo-example:$1-k8s-vault --build-arg "$3" --build-arg "PORT=8081" --build-arg "argBasedVersion=$1" --build-arg "spring_profile=kubernetes-vault" --push ./../../.
+docker buildx build --platform linux/amd64,linux/arm64 -t jeroenwillemsen/wrongsecrets:$1-no-vault --build-arg "$3" --build-arg "PORT=8081" --build-arg "argBasedVersion=$1" --build-arg "spring_profile=without-vault" --push ./../../.
+docker buildx build --platform linux/amd64,linux/arm64 -t jeroenwillemsen/wrongsecrets:$1-local-vault --build-arg "$3" --build-arg "PORT=8081" --build-arg "argBasedVersion=$1" --build-arg "spring_profile=local-vault" --push ./../../.
+docker buildx build --platform linux/amd64,linux/arm64 -t jeroenwillemsen/wrongsecrets:$1-k8s-vault --build-arg "$3" --build-arg "PORT=8081" --build-arg "argBasedVersion=$1" --build-arg "spring_profile=kubernetes-vault" --push ./../../.
+
 #staging (https://arcane-scrubland-42646.herokuapp.com/)
 echo "Completed docker upload for X86, now taking care of heroku, do yourself: update Dockerfile.web, then run 'heroku container:login' 'heroku container:push --recursive --arg argBasedVersion=$1heroku' and 'heroku container:push --recursive --arg argBasedVersion=$1heroku --app=wrongsecrets' and release both (heroku container:release web --app=wrongsecrets)"
 #want to release? do heroku container:release web --app=wrongsecrets
