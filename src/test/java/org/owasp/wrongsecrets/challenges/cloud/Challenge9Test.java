6ec03726feaa5906238faec8b23f44f2bf59d8bc
Nanne Baars <nanne.baars@owasp.org>
2021-11-25 08:54:51 +0100

    Write test cases for challenges
    Controller handles all challenges in the same way, this way we only have to test this one with a simple challenge.

---
+++ src/test/java/org/owasp/wrongsecrets/challenges/cloud/Challenge9Test.java
@@ -0,0 +1,52 @@
+package org.owasp.wrongsecrets.challenges.cloud;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.io.TempDir;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.owasp.wrongsecrets.ScoreCard;
+import org.owasp.wrongsecrets.challenges.Spoiler;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@ExtendWith(MockitoExtension.class)
+class Challenge9Test {
+
+    @Mock
+    private ScoreCard scoreCard;
+
+    @Test
+    void solveChallenge9WithoutFile(@TempDir Path dir) throws Exception {
+        var challenge = new Challenge9(scoreCard, dir.toString(), "test", "k8s");
+
+        Assertions.assertThat(challenge.answerCorrect("secretvalueWitFile")).isFalse();
+    }
+
+    @Test
+    void solveChallenge9WithAWSFile(@TempDir Path dir) throws Exception {
+        var testFile = new File(dir.toFile(), "wrongsecret");
+        var secret = "secretvalueWitFile";
+        Files.writeString(testFile.toPath(), secret);
+
+        var challenge = new Challenge9(scoreCard, dir.toString(), "test", "k8s");
+
+        Assertions.assertThat(challenge.answerCorrect("secretvalueWitFile")).isTrue();
+    }
+
+    @Test
+    void spoilShouldReturnCorrectAnswer(@TempDir Path dir) throws IOException {
+        var testFile = new File(dir.toFile(), "wrongsecret");
+        var secret = "secretvalueWitFile";
+        Files.writeString(testFile.toPath(), secret);
+
+        var challenge = new Challenge9(scoreCard, dir.toString(), "test", "k8s");
+
+        Assertions.assertThat(challenge.spoiler()).isEqualTo(new Spoiler("secretvalueWitFile"));
+    }
+
+}
