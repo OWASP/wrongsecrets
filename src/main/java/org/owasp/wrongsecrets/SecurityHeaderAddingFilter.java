783ab7d5f00e9c7498e1475e9ce7532a874c8707
Jeroen Willemsen <jwillemsen@xebia.com>
2021-12-07 21:21:14 +0100

    adding security headers

---
+++ src/main/java/org/owasp/wrongsecrets/SecurityHeaderAddingFilter.java
@@ -0,0 +1,22 @@
+package org.owasp.wrongsecrets;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class SecurityHeaderAddingFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) response;
+        res.addHeader("Server", "WrongSecrets - Star us!");
+        res.addHeader("X-Frame-Options", "SAMEORIGIN");
+        res.addHeader("X-Content-Type-Options", "nosniff");
+        res.addHeader("Content-Security-Policy", "default-src * 'self' https://wrongsecrets.herokuapp.com/; script-src  * 'self' 'unsafe-inline'; style-src * 'self' 'unsafe-inline'; img-src data:");
+        chain.doFilter(request, res);
+    }
+}
