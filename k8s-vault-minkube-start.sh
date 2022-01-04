fc94685e3292d11c1b95fa38180ded8892c817e6
Jeroen Willemsen <jwillemsen@xebia.com>
2022-01-04 15:07:08 +0100

    updated minikube script to have some logs at bootup

--- k8s-vault-minkube-start.sh
+++ k8s-vault-minkube-start.sh
@@ -116,6 +116,7 @@ echo "Deploy secret challenge app"
 kubectl apply -f k8s/secret-challenge-vault-deployment.yml
 while [[ $(kubectl get pods -l app=secret-challenge -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for secret-challenge" && sleep 2; done
 kubectl expose deployment secret-challenge --type=LoadBalancer --port=8080
+kubectl logs -l app=secret-challenge
 kubectl port-forward \
     $(kubectl get pod -l app=secret-challenge -o jsonpath="{.items[0].metadata.name}") \
     8080:8080 \
