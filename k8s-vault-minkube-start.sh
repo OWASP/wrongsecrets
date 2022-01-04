03b7dc1b7200dc23bb62de95aad49bcbbc18fd19
Jeroen Willemsen <jwillemsen@xebia.com>
2022-01-04 15:57:25 +0100

    adding logs to make sure part 2

--- k8s-vault-minkube-start.sh
+++ k8s-vault-minkube-start.sh
@@ -115,6 +115,7 @@ kubectl exec vault-0 -- vault write auth/kubernetes/role/secret-challenge \
 echo "Deploy secret challenge app"
 kubectl apply -f k8s/secret-challenge-vault-deployment.yml
 while [[ $(kubectl get pods -l app=secret-challenge -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for secret-challenge" && sleep 2; done
+kubectl logs -l app=secret-challenge -f >> pod.log &
 kubectl expose deployment secret-challenge --type=LoadBalancer --port=8080
 kubectl port-forward \
     $(kubectl get pod -l app=secret-challenge -o jsonpath="{.items[0].metadata.name}") \
@@ -125,4 +126,5 @@ echo "wait 20 seconds so we can check if vault-k8s-container works"
 sleep 20
 curl http://localhost:8080/spoil-7
 echo "logs from pod to make sure:"
-kubectl logs -l app=secret-challenge
+cat pod.log
+
