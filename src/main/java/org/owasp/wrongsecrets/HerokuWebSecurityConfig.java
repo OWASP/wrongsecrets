0150b06206f4b5f4a7be1388d0888c00e63fc858
Nanne Baars <nanne.baars@owasp.org>
2022-02-01 20:24:32 +0100

    Fix failing test
    Due to the spring-security configuration for Heroku, Spring security automatically enables basic authentication. We need to explicit disable this.

--- src/main/java/org/owasp/wrongsecrets/HerokuWebSecurityConfig.java
+++ src/main/java/org/owasp/wrongsecrets/HerokuWebSecurityConfig.java
@@ -1,14 +1,18 @@
 package org.owasp.wrongsecrets;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 public class HerokuWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.requiresChannel()
             .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
-            .requiresSecure();
+            .requiresSecure()
+            .and()
+            .httpBasic().disable();
     }
 }
