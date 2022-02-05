14a554a42fe55f9f7b164e502fc721b190f5ee79
Jeroen Willemsen <jwillemsen@xebia.com>
2022-02-05 22:14:21 +0100

    Added challenge 13 for #144

--- src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge13.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge13.java
@@ -9,11 +9,17 @@ import org.owasp.wrongsecrets.challenges.Spoiler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.List;
 
 @Slf4j
 @Component
-public class Challenge15 extends Challenge {
+public class Challenge13 extends Challenge {
 
     private String plainText;
     private String cipherText;
@@ -23,7 +29,7 @@ public class Challenge15 extends Challenge {
         return null;
     }
 
-    public Challenge15(ScoreCard scoreCard, @Value("${plainText}") String plainText, @Value("${cipherText}") String cipherText) {
+    public Challenge13(ScoreCard scoreCard, @Value("${plainText13}") String plainText, @Value("${cipherText13}") String cipherText) {
         super(scoreCard);
         this.plainText = plainText;
         this.cipherText = cipherText;
@@ -46,9 +52,21 @@ public class Challenge15 extends Challenge {
         }
 
         try {
-
-            return true;
+            final byte[] keyData = Base64.getDecoder().decode(base64EncodedKey);
+            byte[] key = new byte[16];
+            System.arraycopy(keyData, 0, key, 0, 16);
+            Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+            SecretKeySpec keySpec = new SecretKeySpec(key, "AES");
+            int GCM_TAG_LENGTH = 16;
+            int GCM_IV_LENGTH = 12;
+            byte[] IV = new byte[GCM_IV_LENGTH];
+            Arrays.fill(IV, (byte) 0);
+            GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(GCM_TAG_LENGTH * 8, IV);
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec, gcmParameterSpec);
+            byte[] cipherTextBytes = cipher.doFinal(plainText.getBytes(StandardCharsets.UTF_8));
+            return cipherText.equals(Base64.getEncoder().encodeToString(cipherTextBytes));
         } catch (Exception e) {
+            log.error("Exception with challnege 13", e);
             return false;
         }
 
