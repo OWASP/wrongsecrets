1208b4505c11e9383e7345a24e0b6990499f92c9
Jeroen Willemsen <jwillemsen@xebia.com>
2022-02-05 07:59:46 +0100

    Update POM file with new version

--- .github/scripts/docker-create-and-push.sh
+++ .github/scripts/docker-create-and-push.sh
@@ -32,20 +32,34 @@ openssl rand -base64 32 | tr -d '\n' > yourkey.txt
 echo "tagging version"
 #git tag -a $tag -m "$1"
 #git push --tags
-#
+
+# preps for #178:
+#echo "Building and publishing to maven central, did you set: a settings.xml file with:"
+#echo "<settings>"
+#echo "  <servers>"
+#echo "    <server>"
+#echo "      <id>ossrh</id>"
+#echo "      <username>your-jira-id</username>"
+#echo "      <password>your-jira-pwd</password>"
+#echo "    </server>"
+#echo "  </servers>"
+#echo "</settings>"
+
+echo "Building and updating pom.xml file so we can use it in our docker"
 cd ../.. && mvn clean install && mvn --batch-mode release:update-versions -DdevelopmentVersion=${tag}-SNAPSHOT
-#git commit -am "Update POM file with new version"
-#
-#docker buildx create --name mybuilder
-#docker buildx use mybuilder
-#echo "creating containers"
-#docker buildx build --platform linux/amd64,linux/arm64 -t jeroenwillemsen/addo-example:$tag-no-vault --build-arg "$2" --build-arg "PORT=8081" --build-arg "argBasedVersion=$tag" --build-arg "spring_profile=without-vault" --push ./../../.
-#docker buildx build --platform linux/amd64,linux/arm64 -t jeroenwillemsen/addo-example:$tag-local-vault --build-arg "$2" --build-arg "PORT=8081" --build-arg "argBasedVersion=$tag" --build-arg "spring_profile=local-vault" --push ./../../.
-#docker buildx build --platform linux/amd64,linux/arm64 -t jeroenwillemsen/addo-example:$tag-k8s-vault --build-arg "$2" --build-arg "PORT=8081" --build-arg "argBasedVersion=$tag" --build-arg "spring_profile=kubernetes-vault" --push ./../../.
-#docker buildx build --platform linux/amd64,linux/arm64 -t jeroenwillemsen/wrongsecrets:$tag-no-vault --build-arg "$2" --build-arg "PORT=8081" --build-arg "argBasedVersion=$tag" --build-arg "spring_profile=without-vault" --push ./../../.
-#docker buildx build --platform linux/amd64,linux/arm64 -t jeroenwillemsen/wrongsecrets:$tag-local-vault --build-arg "$2" --build-arg "PORT=8081" --build-arg "argBasedVersion=$tag" --build-arg "spring_profile=local-vault" --push ./../../.
-#docker buildx build --platform linux/amd64,linux/arm64 -t jeroenwillemsen/wrongsecrets:$tag-k8s-vault --build-arg "$2" --build-arg "PORT=8081" --build-arg "argBasedVersion=$tag" --build-arg "spring_profile=kubernetes-vault" --push ./../../.
-#
-##staging (https://arcane-scrubland-42646.herokuapp.com/)
-#echo "Completed docker upload for X86, now taking care of heroku, do yourself: update Dockerfile.web, then run 'heroku container:login' 'heroku container:push --recursive --arg argBasedVersion=${tag}heroku' and 'heroku container:push --recursive --arg argBasedVersion=${tag}heroku --app=wrongsecrets' and release both (heroku container:release web --app=wrongsecrets)"
-##want to release? do heroku container:release web --app=wrongsecrets
+git add ../../pomx.ml
+git commit -am "Update POM file with new version"
+
+docker buildx create --name mybuilder
+docker buildx use mybuilder
+echo "creating containers"
+docker buildx build --platform linux/amd64,linux/arm64 -t jeroenwillemsen/addo-example:$tag-no-vault --build-arg "$2" --build-arg "PORT=8081" --build-arg "argBasedVersion=$tag" --build-arg "spring_profile=without-vault" --push ./../../.
+docker buildx build --platform linux/amd64,linux/arm64 -t jeroenwillemsen/addo-example:$tag-local-vault --build-arg "$2" --build-arg "PORT=8081" --build-arg "argBasedVersion=$tag" --build-arg "spring_profile=local-vault" --push ./../../.
+docker buildx build --platform linux/amd64,linux/arm64 -t jeroenwillemsen/addo-example:$tag-k8s-vault --build-arg "$2" --build-arg "PORT=8081" --build-arg "argBasedVersion=$tag" --build-arg "spring_profile=kubernetes-vault" --push ./../../.
+docker buildx build --platform linux/amd64,linux/arm64 -t jeroenwillemsen/wrongsecrets:$tag-no-vault --build-arg "$2" --build-arg "PORT=8081" --build-arg "argBasedVersion=$tag" --build-arg "spring_profile=without-vault" --push ./../../.
+docker buildx build --platform linux/amd64,linux/arm64 -t jeroenwillemsen/wrongsecrets:$tag-local-vault --build-arg "$2" --build-arg "PORT=8081" --build-arg "argBasedVersion=$tag" --build-arg "spring_profile=local-vault" --push ./../../.
+docker buildx build --platform linux/amd64,linux/arm64 -t jeroenwillemsen/wrongsecrets:$tag-k8s-vault --build-arg "$2" --build-arg "PORT=8081" --build-arg "argBasedVersion=$tag" --build-arg "spring_profile=kubernetes-vault" --push ./../../.
+
+#staging (https://arcane-scrubland-42646.herokuapp.com/)
+echo "Completed docker upload for X86, now taking care of heroku, do yourself: update Dockerfile.web, then run 'heroku container:login' 'heroku container:push --recursive --arg argBasedVersion=${tag}heroku' and 'heroku container:push --recursive --arg argBasedVersion=${tag}heroku --app=wrongsecrets' and release both (heroku container:release web --app=wrongsecrets)"
+#want to release? do heroku container:release web --app=wrongsecrets
