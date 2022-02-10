a0975bcca7b1aff9d234449393b76d315ec18a5d
Jeroen Willemsen <jwillemsen@xebia.com>
2022-02-10 06:34:01 +0100

    Adding more code for a first prototype of #187: todo, add challengeTexts and add tests

--- src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge16.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge16.java
@@ -5,6 +5,9 @@ import org.apache.logging.log4j.util.Strings;
 import org.linguafranca.pwdb.Database;
 import org.linguafranca.pwdb.kdbx.KdbxCreds;
 import org.linguafranca.pwdb.kdbx.simple.SimpleDatabase;
+import org.linguafranca.pwdb.kdbx.simple.SimpleEntry;
+import org.linguafranca.pwdb.kdbx.simple.SimpleGroup;
+import org.linguafranca.pwdb.kdbx.simple.SimpleIcon;
 import org.owasp.wrongsecrets.RuntimeEnvironment;
 import org.owasp.wrongsecrets.ScoreCard;
 import org.owasp.wrongsecrets.challenges.Challenge;
@@ -12,29 +15,25 @@ import org.owasp.wrongsecrets.challenges.Spoiler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.Cipher;
-import javax.crypto.spec.GCMParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.Base64;
 import java.util.List;
 
 @Slf4j
 @Component
 public class Challenge16 extends Challenge {
 
-    private String keepassxPassword;
+    private final String keepassxPassword;
+    private final String defaultKeepassValue;
 
     @Override
     public Spoiler spoiler() {
-        return null;
+        return new Spoiler(findAnswer());
     }
 
-    public Challenge16(ScoreCard scoreCard, @Value("${plainText13}") String keepassxPassword) {
+    public Challenge16(ScoreCard scoreCard, @Value("${keepasxpassword}") String keepassxPassword, @Value("${KEEPASS_BROKEN}") String defaultKeepassValue) {
         super(scoreCard);
         this.keepassxPassword = keepassxPassword;
+        this.defaultKeepassValue = defaultKeepassValue;
     }
 
     @Override
@@ -47,22 +46,28 @@ public class Challenge16 extends Challenge {
         return List.of(RuntimeEnvironment.Environment.DOCKER);
     }
 
+    private String findAnswer() {
+        if (Strings.isEmpty(keepassxPassword)) {
+            log.info("Checking secret with values {}", keepassxPassword);
+            return defaultKeepassValue;
+        }
+        try {
+            KdbxCreds creds = new KdbxCreds(keepassxPassword.getBytes());
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream("test1.kdbx");
+            Database<SimpleDatabase, SimpleGroup, SimpleEntry, SimpleIcon> database = SimpleDatabase.load(creds, inputStream);
+            return database.findEntries("alibaba").stream().findFirst().toString();
+        } catch (Exception e) {
+            log.error("Exception with challnege 16", e);
+            return defaultKeepassValue;
+        }
+    }
+
+    //todo: write a test!
     private boolean isanswerCorrectInKeeyPassx(String answer) {
         if (Strings.isEmpty(keepassxPassword) || Strings.isEmpty(answer)) {
             log.info("Checking secret with values {}, {}", keepassxPassword, answer);
             return false;
         }
-
-        try {
-            KdbxCreds creds = new KdbxCreds(keepassxPassword.getBytes());
-            InputStream inputStream = getClass().getClassLoader().getResourceAsStream("test1.kdbx");
-            Database database = SimpleDatabase.load(creds, inputStream);
-            //todo implement keepassreading!
-            return false;
-        } catch (Exception e) {
-            log.error("Exception with challnege 16", e);
-            return false;
-        }
-
+        return answer.equals(findAnswer());
     }
 }
