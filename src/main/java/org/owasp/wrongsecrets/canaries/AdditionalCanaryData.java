e90c3f86688fff4d9251f4b828ca5244637b0125
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2022-03-07 07:18:38 +0100

    Correctiosn for callback

--- src/main/java/org/owasp/wrongsecrets/canaries/AdditionalCanaryData.java
+++ src/main/java/org/owasp/wrongsecrets/canaries/AdditionalCanaryData.java
@@ -7,9 +7,15 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public class AdditionalCanaryData {
 
-    private final String srcIp;
+    private final String src_ip;
     private final String useragent;
     private final String referer;
     private final String location;
 
 }
+/*
+{"manage_url": "http://canarytokens.org/manage?token=y0all60b627gzp19ahqh7rl6j&auth=09193ea6b8def3e27a1a41f98d4265d7",
+ "memo": "debugtoken",
+ "additional_data": {"src_ip": "83.128.90.255", "useragent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36", "referer": null, "location": null},
+ "channel": "HTTP", "time": "2022-03-07 06:14:36 (UTC)"}
+ */
--- src/main/java/org/owasp/wrongsecrets/canaries/CanaryToken.java
+++ src/main/java/org/owasp/wrongsecrets/canaries/CanaryToken.java
@@ -6,10 +6,16 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public class CanaryToken {
-    private final String manageUrl;
+    private final String manage_url;
     private final String memo;
     private final String channel;
     private final String time;
-    private final AdditionalCanaryData additionalData;
+    private final AdditionalCanaryData additional_data;
 }
 
+/*
+{"manage_url": "http://canarytokens.org/manage?token=y0all60b627gzp19ahqh7rl6j&auth=09193ea6b8def3e27a1a41f98d4265d7",
+ "memo": "debugtoken",
+ "additional_data": {"src_ip": "83.128.90.255", "useragent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36", "referer": null, "location": null},
+ "channel": "HTTP", "time": "2022-03-07 06:14:36 (UTC)"}
+ */
