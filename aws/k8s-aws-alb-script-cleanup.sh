3341380b2a0fb87769c8f86d4786ba21ebae2974
Ben de Haan <53901866+bendehaan@users.noreply.github.com>
2022-03-16 14:26:22 +0100

    Also change version in cleanup script

--- aws/k8s-aws-alb-script-cleanup.sh
+++ aws/k8s-aws-alb-script-cleanup.sh
@@ -24,9 +24,8 @@ fi
 ACCOUNT_ID=$(aws sts get-caller-identity | jq '.Account' -r)
 echo "ACCOUNT_ID=${ACCOUNT_ID}"
 
-LBC_VERSION="v2.3.0"
+LBC_VERSION="v2.4.0"
 echo "LBC_VERSION=$LBC_VERSION"
-
 
 echo "cleanup k8s ingress and service"
 kubectl delete ingress wrongsecrets
@@ -34,19 +33,17 @@ kubectl delete service secret-challenge
 
 echo "Cleanup helm chart"
 helm uninstall aws-load-balancer-controller \
-    -n kube-system
+  -n kube-system
 
 echo "Cleanup k8s ALB"
 kubectl delete -k "github.com/aws/eks-charts/stable/aws-load-balancer-controller//crds?ref=master"
 
-
-
 echo "Cleanup iam serviceaccount and policy"
 eksctl delete iamserviceaccount \
-    --cluster $CLUSTERNAME \
-    --name aws-load-balancer-controller \
-    --namespace kube-system \
-    --region $AWS_REGION
+  --cluster $CLUSTERNAME \
+  --name aws-load-balancer-controller \
+  --namespace kube-system \
+  --region $AWS_REGION
 
 aws iam delete-policy \
-    --policy-arn arn:aws:iam::${ACCOUNT_ID}:policy/AWSLoadBalancerControllerIAMPolicy
+  --policy-arn arn:aws:iam::${ACCOUNT_ID}:policy/AWSLoadBalancerControllerIAMPolicy
