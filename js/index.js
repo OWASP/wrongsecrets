3ab60845e1ddb6311d394933b0075c5ad473b846
Marcin Nowak <marcin.nowak@techmetria.pl>
2022-04-02 11:35:13 +0200

    Update POM file with new version: 1.3.10

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 function secret() { 
- var password = "jQNPAo4=" + 9 + "5/6A" + 6 + "0kQ=" + 2 + "pFbS" + 7;
+ var password = "rzA0yQs=" + 9 + "heb4" + 6 + "Zuw=" + 2 + "i+em" + 7;
  return password;
  }
