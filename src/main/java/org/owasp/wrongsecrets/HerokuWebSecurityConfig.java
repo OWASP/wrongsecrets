42d26f064d7af288bd8e87fd3bc975aeabba343f
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2022-04-03 00:49:18 +0200

    attempt to fix the redirect

--- src/main/java/org/owasp/wrongsecrets/HerokuWebSecurityConfig.java
+++ src/main/java/org/owasp/wrongsecrets/HerokuWebSecurityConfig.java
@@ -1,20 +1,20 @@
 package org.owasp.wrongsecrets;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity(debug = true)
+@Order(0)
 public class HerokuWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.requiresChannel()
-            .requestMatchers(r -> r.getHeader("x-forwarded-proto") != null)
-            .requiresSecure()
-            .and()
-            .httpBasic().disable();
+            .requestMatchers(r -> r.getHeader("x-forwarded-proto") != null || r.getHeader("X-Forwarded-Proto") != null)
+            .requiresSecure();
     }
 }
--- src/main/java/org/owasp/wrongsecrets/canaries/TokenCallbackSecurityConfiguration.java
+++ src/main/java/org/owasp/wrongsecrets/canaries/TokenCallbackSecurityConfiguration.java
@@ -1,17 +1,19 @@
-package org.owasp.wrongsecrets;
+package org.owasp.wrongsecrets.canaries;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity(debug = true)
+@Order(1)
 public class TokenCallbackSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.requestMatcher(r -> r.getRequestURI().contains("canaries/tokencallback"))
-            .csrf().disable();
+        http.authorizeRequests().requestMatchers(r -> r.getRequestURL().toString().contains("canaries")).permitAll()
+            .and().csrf().disable();
     }
 }
