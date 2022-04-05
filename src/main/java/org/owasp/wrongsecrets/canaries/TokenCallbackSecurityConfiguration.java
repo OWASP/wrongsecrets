b57c98ec4d840a114324234ba424d8e8e6cd89d0
Marcin Nowak <marcin.nowak@techmetria.pl>
2022-04-05 22:17:56 +0200

    #44 front end JavaScript library with key obfuscated - review

--- src/main/java/org/owasp/wrongsecrets/canaries/TokenCallbackSecurityConfiguration.java
+++ src/main/java/org/owasp/wrongsecrets/canaries/TokenCallbackSecurityConfiguration.java
@@ -3,7 +3,6 @@ package org.owasp.wrongsecrets.canaries;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -12,6 +11,8 @@ public class TokenCallbackSecurityConfiguration extends WebSecurityConfigurerAda
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.requestMatcher(r -> r.getRequestURL().toString().contains("canaries")).csrf().disable();
+        http.requestMatcher(r ->
+                r.getRequestURL().toString().contains("canaries") || r.getRequestURL().toString().contains("token"))
+            .csrf().disable();
     }
 }
