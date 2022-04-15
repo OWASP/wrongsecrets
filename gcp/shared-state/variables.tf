80c1e888a44a8fee12ecf9bcbdcba3955514906c
tiborhercz <tibor@hercz.nl>
2022-04-15 12:44:57 +0200

    Add gsc shared backend
    - Add gsc shared backend
    - Fix typo in Azure and AWS README
    - Fix path in the ingress-clean script

---
+++ gcp/shared-state/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+    description = "The GCP project id to use"
+    type        = string
+    default     = "owasp-wrongsecrets"
+}
+
+variable "region" {
+    description = "The GCP region to use"
+    type        = string
+    default     = "europe-west4"
+}
