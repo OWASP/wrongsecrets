5d082cb45912e2b881f185b3635d895d15aff9c8
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2022-05-12 11:34:34 +0200

    Update POM file with new version: 1.3.13

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 function secret() { 
- var password = "um0gr/M=" + 9 + "6MiN" + 6 + "jrI=" + 2 + "8UHN" + 7;
+ var password = "cVmWSqY=" + 9 + "OxMk" + 6 + "vK0=" + 2 + "dadq" + 7;
  return password;
  }
