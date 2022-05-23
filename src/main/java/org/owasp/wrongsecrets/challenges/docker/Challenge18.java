694da8b5b5d38cbbc5d62177a6ba16908405eed8
Joss Sparkes <joss.sparkes@gmail.com>
2022-05-23 22:53:46 +0100

    Added first draft of challenge
    Signed-off-by: Joss Sparkes <joss.sparkes@gmail.com>

---
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge18.java
@@ -0,0 +1,36 @@
+package org.owasp.wrongsecrets.challenges.docker;
+
+
+import org.owasp.wrongsecrets.RuntimeEnvironment;
+import org.owasp.wrongsecrets.ScoreCard;
+import org.owasp.wrongsecrets.challenges.Challenge;
+import org.owasp.wrongsecrets.challenges.Spoiler;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static org.owasp.wrongsecrets.RuntimeEnvironment.Environment.DOCKER;
+
+@Component
+@Order(1)
+public class Challenge18 extends Challenge {
+
+    public Challenge18(ScoreCard scoreCard) {
+        super(scoreCard);
+    }
+
+    @Override
+    public Spoiler spoiler() {
+        return new Spoiler("hunter2");
+    }
+
+    @Override
+    public boolean answerCorrect(String answer) {
+        return "hunter2".equals(answer);
+    }
+
+    public List<RuntimeEnvironment.Environment> supportedRuntimeEnvironments() {
+        return List.of(DOCKER);
+    }
+}
