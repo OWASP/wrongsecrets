e33de710d0cc70d72d49521a520cf737323c9f9f
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2022-06-26 10:36:30 +0200

    Update POM file with new version: challenge21test

---
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge21.java
@@ -0,0 +1,42 @@
+package org.owasp.wrongsecrets.challenges.docker;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.owasp.wrongsecrets.RuntimeEnvironment;
+import org.owasp.wrongsecrets.ScoreCard;
+import org.owasp.wrongsecrets.challenges.Challenge;
+import org.owasp.wrongsecrets.challenges.Spoiler;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static org.owasp.wrongsecrets.RuntimeEnvironment.Environment.DOCKER;
+
+@Component
+@Order(21)
+@Slf4j
+public class Challenge21 extends Challenge {
+
+    private final BinaryExecutionHelper binaryExecutionHelper;
+
+    public Challenge21(ScoreCard scoreCard) {
+        super(scoreCard);
+        this.binaryExecutionHelper = new BinaryExecutionHelper(21);
+    }
+
+
+    @Override
+    public Spoiler spoiler() {
+        return new Spoiler(binaryExecutionHelper.executeCommand("", "wrongsecrets-golang"));
+    }
+
+    @Override
+    public boolean answerCorrect(String answer) {
+        return binaryExecutionHelper.executeGoCommand(answer).equals("This is correct! Congrats!");
+    }
+
+    public List<RuntimeEnvironment.Environment> supportedRuntimeEnvironments() {
+        return List.of(DOCKER);
+    }
+}
