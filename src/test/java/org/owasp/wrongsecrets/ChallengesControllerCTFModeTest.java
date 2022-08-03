096607f38beb048f92f16cd6ef0653fe8ca4e0a7
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2022-08-03 14:36:12 +0200

    fix testing, remove junit, thank @nbaars

--- src/test/java/org/owasp/wrongsecrets/ChallengesControllerCTFModeTest.java
+++ src/test/java/org/owasp/wrongsecrets/ChallengesControllerCTFModeTest.java
@@ -1,41 +1,24 @@
 package org.owasp.wrongsecrets;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.owasp.wrongsecrets.RuntimeEnvironment.Environment;
-import org.owasp.wrongsecrets.challenges.*;
+import org.owasp.wrongsecrets.challenges.ChallengeForm;
 import org.owasp.wrongsecrets.challenges.docker.Challenge1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.client.RestClientResponseException;
 
-import javax.ws.rs.core.Application;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
-import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.contains;
-import static org.mockito.Mockito.when;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.MOCK;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(
     properties = {"CTF_ENABLED=true", "CTF_KEY=randomtextforkey"},
     classes = WrongSecretsApplication.class
@@ -57,13 +40,14 @@ class ChallengesControllerCTFModeTest {
 
     @Test
     void shouldShowFlagWhenRespondingWithSuccessInCTFMode() throws Exception {
-        var spoil = new ChallengeForm(new Challenge1(new InMemoryScoreCard(1)).spoiler().solution()).toString();
-        mvc.perform(post("/challenges/1")
+        var spoil = new Challenge1(new InMemoryScoreCard(1)).spoiler().solution();
+        mvc.perform(post("/challenge/1")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .param("solution", spoil)
                 .param("action", "submit")
-                .param("csrf","fd6aae2e-e85b-4c52-96f3-d71c6c725d11"))
-            .andExpect(status().isOk());
+                .with(csrf()))
+            .andExpect(status().isOk())
+            .andExpect(content().string(containsString("ba9a72ac7057576344856")));
 
     }
 }
