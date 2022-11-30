eeadd8fe02460dce2c91c2911d72fb1c8a81d34a
Nowak M. (Marcin) <marcin.nowak3@ing.com>
2022-11-30 22:06:29 +0100

    Replace depreciated configuration api

--- src/main/java/org/owasp/wrongsecrets/ActuatorecurityConfiguration.java
+++ src/main/java/org/owasp/wrongsecrets/ActuatorecurityConfiguration.java
@@ -3,10 +3,11 @@ package org.owasp.wrongsecrets;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @Order(2)
-public class ActuatorecurityConfiguration extends HerokuWebSecurityConfig {
+public class ActuatorecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
