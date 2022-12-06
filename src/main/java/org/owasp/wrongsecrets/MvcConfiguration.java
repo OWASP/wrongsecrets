38a674554f42a3b1c4088e73802e15543ef502d1
Marcin Nowak <marcin.nowak@techmetria.pl>
2022-12-06 22:54:09 +0100

    Spring Boot 3.0 migration - clean up

--- src/main/java/org/owasp/wrongsecrets/MvcConfiguration.java
+++ src/main/java/org/owasp/wrongsecrets/MvcConfiguration.java
@@ -11,10 +11,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.extras.springsecurity6.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.FileTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
@@ -41,15 +39,6 @@ public class MvcConfiguration implements WebMvcConfigurer {
     }
 
     @Bean
-    public ThymeleafViewResolver viewResolver(SpringTemplateEngine templateEngine) {
-        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-        viewResolver.setTemplateEngine(templateEngine);
-        viewResolver.setOrder(1);
-        viewResolver.setViewNames(new String[]{".html", ".xhtml"});
-        return viewResolver;
-    }
-
-    @Bean
     public TemplateGenerator generator(@Value("${asciidoctor.enabled}") boolean asciiDoctorEnabled) {
         if (asciiDoctorEnabled) {
             return new AsciiDocGenerator();
@@ -72,7 +61,6 @@ public class MvcConfiguration implements WebMvcConfigurer {
         SpringTemplateEngine engine = new SpringTemplateEngine();
         engine.setEnableSpringELCompiler(true);
         engine.addDialect(new LayoutDialect());
-        engine.addDialect(new SpringSecurityDialect());
         engine.setTemplateResolvers(
             Set.of(asciiDoctorTemplateResolver, springThymeleafTemplateResolver));
         return engine;
