0eaaebdf4ec7f45111731d3c40e771572199bcb9
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2022-12-31 09:04:53 +0100

    Fix for tests

--- src/test/java/org/owasp/wrongsecrets/HerokuWebSecurityConfigTest.java
+++ src/test/java/org/owasp/wrongsecrets/HerokuWebSecurityConfigTest.java
@@ -1,5 +1,6 @@
 package org.owasp.wrongsecrets;
 
+import org.apache.hc.client5.http.HttpHostConnectException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,6 +8,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.ResourceAccessException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -31,9 +33,15 @@ public class HerokuWebSecurityConfigTest {
             .defaultHeader("x-forwarded-proto", "value")
             .build();
         var rootAddress = "http://localhost:" + port + "/heroku";//note we loosely ask for "heroku" to be part of the url
-        var result = restTemplate.getForEntity(rootAddress, String.class);
-        assertEquals(HttpStatus.FOUND, result.getStatusCode());
-        assertEquals("https", result.getHeaders().getLocation().getScheme());
+        try {
+            //old, but will fail in srping security 2022
+            var result = restTemplate.getForEntity(rootAddress, String.class);
+            assertEquals(HttpStatus.FOUND, result.getStatusCode());
+            assertEquals("https", result.getHeaders().getLocation().getScheme());
+        } catch (ResourceAccessException e) {
+            assertEquals(e.getCause().getClass(), HttpHostConnectException.class);
+            assertEquals(e.getCause().getMessage(), "Connect to https://localhost:8443 [localhost/127.0.0.1, localhost/0:0:0:0:0:0:0:1] failed: Connection refused");
+        }
     }
 
     @Test
