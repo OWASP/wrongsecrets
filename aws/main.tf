6c923745b34cfe8f8fc129a9f47fc3801a00088f
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2023-01-02 09:30:49 +0100

    Fixes for public api access in version 18 of tf provider

--- aws/main.tf
+++ aws/main.tf
@@ -72,6 +72,7 @@ module "eks" {
 
 
   cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
 
   cluster_endpoint_public_access_cidrs = ["${data.http.ip.response_body}/32"]
 
