663cdbb7e536079379e26d08ab555aabd93a395c
Marcin Nowak <marcin.nowak@techmetria.pl>
2023-01-16 18:15:16 +0100

    Fixing Heroku test redirection to HTTPS based on suggestion in  https://github.com/spring-projects/spring-boot/issues/33451

--- src/test/java/org/owasp/wrongsecrets/ChallengeAPiControllerTest.java
+++ src/test/java/org/owasp/wrongsecrets/ChallengeAPiControllerTest.java
@@ -3,9 +3,8 @@ package org.owasp.wrongsecrets;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.RestClientResponseException;
 
@@ -13,7 +12,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Import(ConventionPortMapper.class)
 class ChallengeAPiControllerTest {
     @LocalServerPort
     private int port;
@@ -28,14 +26,14 @@ class ChallengeAPiControllerTest {
     void shouldGetListOfChallenges() {
         var restTemplate = builder.build();
 
-        var callbackAdress = "http://localhost:"+port+"/api/Challenges";
+        var callbackAdress = "http://localhost:" + port + "/api/Challenges";
 
         try {
             var response = restTemplate.getForEntity(callbackAdress, String.class);
             assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
             assertThat(response.getBody()).contains("hint");
         } catch (RestClientResponseException e) {
-           fail(e);
+            fail(e);
         }
     }
 }
--- src/test/java/org/owasp/wrongsecrets/PortConfiguration.java
+++ src/test/java/org/owasp/wrongsecrets/PortConfiguration.java
@@ -1,19 +1,19 @@
 package org.owasp.wrongsecrets;
 
 import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.PortMapper;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+@TestConfiguration
 public class PortConfiguration {
     @Bean
     @Order(1)
     public SecurityFilterChain configureHerokuWebSecurityTest(HttpSecurity http,
-                                                          ObjectProvider<PortMapper> portMapper) throws Exception {
+                                                              ObjectProvider<PortMapper> portMapper) throws Exception {
         portMapper.ifAvailable(http.portMapper()::portMapper);
         http.requiresChannel()
             .requestMatchers(r ->
