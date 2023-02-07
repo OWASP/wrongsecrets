7dca3f46aff66720b61142d2f5e4ea4b6cdb0131
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2023-02-07 22:03:04 +0100

    fix sound, added missing  musl binaries

--- wrongsecret-desktop-resources/startwm.sh
+++ wrongsecret-desktop-resources/startwm.sh
@@ -3,4 +3,5 @@
 /startpulse.sh &
 cp -r /var/tmp/wrongsecrets /config/Desktop
 cp -r /var/tmp/wrongsecrets/welcome.md /config/Desktop
+sudo chown abc /config/.config/pulse
 /usr/bin/startxfce4 > /dev/null 2>&1
