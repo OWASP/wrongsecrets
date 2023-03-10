64de55435e250af752c740609f7be8ec8899fc5b
Puneeth <pyd773@gmail.com>
2023-03-10 08:52:07 -0800

    Adding all the files related to challenge 28

---
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge28.java
@@ -0,0 +1,62 @@
+package org.owasp.wrongsecrets.challenges.docker;
+
+
+import org.owasp.wrongsecrets.RuntimeEnvironment;
+import org.owasp.wrongsecrets.ScoreCard;
+import org.owasp.wrongsecrets.challenges.Challenge;
+import org.owasp.wrongsecrets.challenges.ChallengeTechnology;
+import org.owasp.wrongsecrets.challenges.Spoiler;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static org.owasp.wrongsecrets.RuntimeEnvironment.Environment.DOCKER;
+
+@Component
+@Order(28)
+public class Challenge28 extends Challenge {
+
+    private String SecretKey="asdfdpuYUIadbaoasdpa07kj2003";
+
+    public Challenge28(ScoreCard scoreCard) {
+        super(scoreCard);
+        this.SecretKey = SecretKey;
+    }
+
+    @Override
+    public boolean canRunInCTFMode() {
+        return true;
+    }
+
+
+    @Override
+    public Spoiler spoiler() {
+        return new Spoiler(SecretKey);
+    }
+
+    @Override
+    public boolean answerCorrect(String answer) {
+        return SecretKey.equals(answer);
+    }
+
+    public List<RuntimeEnvironment.Environment> supportedRuntimeEnvironments() {
+        return List.of(DOCKER);
+    }
+
+    @Override
+    public int difficulty() {
+        return 1;
+    }
+
+    @Override
+    public String getTech() {
+        return ChallengeTechnology.Tech.GIT.id;
+    }
+
+    @Override
+    public boolean isLimittedWhenOnlineHosted() {
+        return false;
+    }
+
+}
