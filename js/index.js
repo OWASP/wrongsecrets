fab510b7fadd62ecb45a968142ca510c3f29dba6
Ben de Haan <53901866+bendehaan@users.noreply.github.com>
2023-03-10 09:10:05 +0100

    feat: add eslint

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-function secret() {
- var password = "jLJIQZM=" + 9 + "aG7l" + 6 + "LAQ=" + 2 + "UdiM" + 7;
- return password;
- }
+/* eslint-disable no-unused-vars */
+function secret () {
+  const password = 'jLJIQZM=' + 9 + 'aG7l' + 6 + 'LAQ=' + 2 + 'UdiM' + 7
+  return password
+}
