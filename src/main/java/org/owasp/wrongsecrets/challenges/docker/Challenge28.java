71db12c36307b7f159192dbcb9378abd5c69ccb6
Puneeth <pyd773@gmail.com>
2023-03-11 18:08:19 +0530

    importing the modules required

--- src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge28.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge28.java
@@ -1,6 +1,7 @@
 package org.owasp.wrongsecrets.challenges.docker;
 
 
+import org.bouncycastle.util.encoders.Base64;
 import org.owasp.wrongsecrets.RuntimeEnvironment;
 import org.owasp.wrongsecrets.ScoreCard;
 import org.owasp.wrongsecrets.challenges.Challenge;
