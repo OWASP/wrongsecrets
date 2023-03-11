83312596ee432d6eeffb51d28ee8a2b554da0d8e
Puneeth <pyd773@gmail.com>
2023-03-11 17:29:12 +0530

    Update src/test/java/org/owasp/wrongsecrets/challenges/docker/Challenge28Test.java
    Co-authored-by: Jeroen Willemsen <jeroenwillemsen2001@gmail.com>

--- src/test/java/org/owasp/wrongsecrets/challenges/docker/Challenge28Test.java
+++ src/test/java/org/owasp/wrongsecrets/challenges/docker/Challenge28Test.java
@@ -14,7 +14,7 @@ class Challenge28Test {
 
     @Mock
     private ScoreCard scoreCard;
-    private String SecretKey="asdfdpuYUIadbaoasdpa07kj2003";
+    private String secretKey=new String(Hex.decode("61736466647075595549616462616f617364706130376b6a32303033"));
     @Test
     void spoilerShouldRevealAnswer() {
         var challenge = new Challenge28(scoreCard);
