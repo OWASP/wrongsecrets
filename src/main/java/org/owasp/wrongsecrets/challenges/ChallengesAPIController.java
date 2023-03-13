2e35405276ecd99b58e71e0f773f341e5c6ef498
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2023-03-13 07:44:29 +0100

    feat(#698): implement spotbugs: package before running & initial first fixes of bugss

--- src/main/java/org/owasp/wrongsecrets/challenges/ChallengesAPIController.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/ChallengesAPIController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -106,7 +107,7 @@ public class ChallengesAPIController {
             var resource = ResourceUtils.getURL(resourceName);
             final StringBuilder resourceStringbuilder = new StringBuilder();
             new BufferedReader(
-                new InputStreamReader(resource.openStream())
+                new InputStreamReader(resource.openStream(), StandardCharsets.UTF_8)
             ).lines().forEach(resourceStringbuilder::append);
             return resourceStringbuilder.toString();
         } catch (IOException e) {
