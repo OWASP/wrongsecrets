e364bb8382ae49cf5d1a28da9f8403f7094f5999
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2023-03-15 10:11:45 +0100

    Feat(#707): updated ui a bit more

--- src/main/java/org/owasp/wrongsecrets/SecurityHeaderAddingFilter.java
+++ src/main/java/org/owasp/wrongsecrets/SecurityHeaderAddingFilter.java
@@ -15,7 +15,7 @@ public class SecurityHeaderAddingFilter implements Filter {
         res.addHeader("Server", "WrongSecrets - Star us!");
         res.addHeader("X-Frame-Options", "SAMEORIGIN");
         res.addHeader("X-Content-Type-Options", "nosniff");
-        res.addHeader("Content-Security-Policy", "default-src * 'self'; script-src  * 'self' 'unsafe-inline'; style-src * 'self' 'unsafe-inline'; img-src data 'self':");
+        res.addHeader("Content-Security-Policy", "default-src * 'self'; script-src  * 'self' 'unsafe-inline'; style-src * 'self' 'unsafe-inline'; img-src data:");
         chain.doFilter(request, res);
     }
 }
