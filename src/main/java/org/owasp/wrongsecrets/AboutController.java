afb1aebfbc61d01f4a4a927bf21651c94908c690
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2023-03-15 06:52:30 +0100

    feat(#707): basic about controllertest

--- src/main/java/org/owasp/wrongsecrets/AboutController.java
+++ src/main/java/org/owasp/wrongsecrets/AboutController.java
@@ -8,32 +8,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class StatsController {
+public class AboutController {
 
-    @Autowired
-    private CanaryCounter canaryCounter;
-    @Autowired
-    private SessionConfiguration sessionConfiguration;
 
-    @Value("${hints_enabled}")
-    private boolean hintsEnabled;
-    @Value("${reason_enabled}")
-    private boolean reasonEnabled;
-    @Value("${ctf_enabled}")
-    private boolean ctfModeEnabled;
-
-    @Value("${canarytokenURLs}")
-    private String[] canaryTokenURLs;
-
-    @GetMapping("/stats")
+    @GetMapping("/about")
     public String getStats(Model model) {
-        model.addAttribute("canaryCounter", canaryCounter.getTotalCount());
-        model.addAttribute("sessioncounter", sessionConfiguration.getCounter());
-        model.addAttribute("lastCanaryToken", canaryCounter.getLastToken());
-        model.addAttribute("canarytokenURLs", canaryTokenURLs);
-        model.addAttribute("hintsEnabled", hintsEnabled);
-        model.addAttribute("reasonEnabled", reasonEnabled);
-        model.addAttribute("ctfModeEnabled",ctfModeEnabled);
-        return "stats";
+        return "about";
     }
 }
