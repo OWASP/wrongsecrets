4e1f59c1648ff723258a9811da9ceac27014364f
TURJO <turjoc120@gmail.com>
2023-03-21 03:17:35 +0600

    save initial preference

--- src/main/resources/static/theme-toggle.js
+++ src/main/resources/static/theme-toggle.js
@@ -1,11 +1,20 @@
 (function () {
+  const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+  const initialTheme = darkModeMediaQuery.matches || localStorage.getItem('darkMode') === 'true';
+
   function updateToggle (darkMode) {
     document.querySelector(".theme-toggle input[type=radio][value='dark']").checked = darkMode
     document.querySelector(".theme-toggle input[type=radio][value='light']").checked = !darkMode
 
     document.body.classList.toggle('dark-mode', darkMode)
-    localStorage.setItem('theme', darkMode ? 'dark' : 'light')
+    localStorage.setItem('darkMode', darkMode)
   }
+
+  darkModeMediaQuery.addEventListener('change', (e) => {
+    const darkModeOn = e.matches
+    updateToggle(darkModeOn)
+  })
 
   window.addEventListener('load', function () {
     const radios = document.querySelectorAll('.theme-toggle input[type=radio]')
@@ -15,11 +24,6 @@
       })
     })
 
-    const storedTheme = localStorage.getItem('theme')
-    if (storedTheme === 'dark') {
-      updateToggle(true)
-    } else {
-      updateToggle(false)
-    }
+    updateToggle(initialTheme)
   })
-})();
+})()
