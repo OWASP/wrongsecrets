ee15821820908121165ab5fa6b5444545e12dd18
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2023-03-26 08:38:05 +0200

    Feat(#680): adding more javadocs

--- src/main/java/org/owasp/wrongsecrets/canaries/CanariesController.java
+++ src/main/java/org/owasp/wrongsecrets/canaries/CanariesController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ *
+ */
 @Slf4j
 @RestController
 public class CanariesController {
