ffdd3da90a7c8dcdeb06bd397a5cf912eda7f448
Novice-expert <95133039+Novice-expert@users.noreply.github.com>
2023-03-27 23:57:24 +0530

    Adds RestController Test.
    Now the maven builds successfully.
    The ChallengeRestControllerTest works fine!

---
+++ src/test/java/org/owasp/wrongsecrets/Challenge29IntegrationTest.java
@@ -0,0 +1,42 @@
+package org.owasp.wrongsecrets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.RestClientResponseException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class Challenge29IntegrationTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private RestTemplateBuilder builder;
+
+    public Challenge29IntegrationTest() {
+    }
+
+    @Test
+    void shouldGetFullintegratedStepsForChallenge29() {
+        var restTemplate = builder.build();
+
+        var secretLoadingAdres = "http://localhost:" + port + "/hidden";
+        var spoilAddress = "http://localhost:" + port + "/spoil-29";
+        try {
+            var response = restTemplate.getForEntity(secretLoadingAdres, String.class);
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+            String answer = response.getBody();
+            var spoilResponse = restTemplate.getForEntity(spoilAddress, String.class);
+            assertThat(spoilResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+            assertThat(spoilResponse.getBody()).contains(answer);
+        } catch (RestClientResponseException e) {
+            fail(e);
+        }
+    }
+}
