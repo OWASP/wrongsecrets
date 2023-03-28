b4ce677d1a3dc5dfc28bef25270df7b356dfbbde
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2023-03-28 08:59:36 +0200

    feat(#680) more docs

--- src/main/java/org/owasp/wrongsecrets/challenges/Spoiler.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/Spoiler.java
@@ -1,8 +1,10 @@
 package org.owasp.wrongsecrets.challenges;
 
 /**
- *
- * @param solution
+ * Class used to return spoilers with the actual answer.
+ * Spoilers are used for quick testing & for finding the answer if a tutorial fails.
+ * Please note that spoiling is disabled in CTF mode, and there is the setting
+ * @param solution the actual solution
  */
 public record Spoiler(String solution) {
 }
