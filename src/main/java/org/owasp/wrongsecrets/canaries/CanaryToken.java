6a7676c9218f03ea8f6f1af35834ed3d19a59213
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2023-03-28 09:09:52 +0200

    feat(#680): fix for javadoc

--- src/main/java/org/owasp/wrongsecrets/canaries/CanaryToken.java
+++ src/main/java/org/owasp/wrongsecrets/canaries/CanaryToken.java
@@ -7,10 +7,20 @@ import lombok.RequiredArgsConstructor;
 /**
  * Actual canaryToken as received by <a href="http://canarytokens.com/">canarytokens.com</a>
  * Example content:
- * {"manage_url": "http://canarytokens.org/manage?token=y0all60b627gzp19ahqh7rl6j&auth=09193ea6b8def3e27a1a41f98d4265d7",
+ * json:
+ * {
+ *  "manage_url": "http://canarytokens.org/manage?token....",
  *  "memo": "debugtoken",
- *  "additional_data": {"src_ip": "83.128.90.255", "useragent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36", "referer": null, "location": null},
- *  "channel": "HTTP", "time": "2022-03-07 06:14:36 (UTC)"}
+ *  "additional_data":
+ *  {
+ *      "src_ip": "83.128.90.255",
+ *      "useragent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+ *      "referer": null,
+ *      "location": null
+ *  },
+ *  "channel": "HTTP",
+ *  "time": "2022-03-07 06:14:36 (UTC)"
+ *  }
  * @see org.owasp.wrongsecrets.canaries.AdditionalCanaryData
  */
 @RequiredArgsConstructor
--- src/main/java/org/owasp/wrongsecrets/challenges/Spoiler.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/Spoiler.java
@@ -2,7 +2,7 @@ package org.owasp.wrongsecrets.challenges;
 
 /**
  * Class used to return spoilers with the actual answer.
- * Spoilers are used for quick testing & for finding the answer if a tutorial fails.
+ * Spoilers are used for quick testing and for finding the answer if a tutorial fails.
  * Please note that spoiling is disabled in CTF mode, and there is the setting
  * @param solution the actual solution
  */
