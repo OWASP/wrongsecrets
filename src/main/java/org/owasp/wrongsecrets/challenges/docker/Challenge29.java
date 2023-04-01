9d6e919bd8193074484aeb020196a41dac044c01
Puneeth <pyd773@gmail.com>
2023-04-01 22:41:51 +0530

    Apply suggestions from code review
    Co-authored-by: Jeroen Willemsen <jeroenwillemsen2001@gmail.com>

--- src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge29.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge29.java
@@ -57,7 +57,7 @@ public class Challenge29 extends Challenge {
 
     @Override
     public String getTech() {
-        return ChallengeTechnology.Tech.GIT.id;
+        return ChallengeTechnology.Tech.DOCUMENTATION.id;
     }
 
     @Override
