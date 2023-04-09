f39650172d63485a69f163a9a0b63e0486deb176
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2023-04-09 07:48:34 +0200

    feat(#680): added more missing javadoc

--- src/main/java/org/owasp/wrongsecrets/FailtoStartupException.java
+++ src/main/java/org/owasp/wrongsecrets/FailtoStartupException.java
@@ -2,6 +2,9 @@ package org.owasp.wrongsecrets;
 
 import org.springframework.boot.ExitCodeGenerator;
 
+/**
+ * Used to give a clear non-0 exit code when the Application cannot start.
+ */
 public class FailtoStartupException extends RuntimeException implements ExitCodeGenerator {
 
     public FailtoStartupException(String message) {
