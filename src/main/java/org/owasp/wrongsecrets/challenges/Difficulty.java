a6c1a567482232ad44f7938915403de29f7f252f
Nanne Baars <nanneb@gmail.com>
2023-04-28 14:45:08 +0200

    review: fix Spotbugs errors

--- src/main/java/org/owasp/wrongsecrets/challenges/Difficulty.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/Difficulty.java
@@ -5,11 +5,11 @@ package org.owasp.wrongsecrets.challenges;
  */
 public class Difficulty {
 
-    public static int EASY = 1;
-    public static int NORMAL = 2;
-    public static int HARD = 3;
-    public static int EXPERT = 4;
-    public static int MASTER = 5;
+    public static final int EASY = 1;
+    public static final int NORMAL = 2;
+    public static final int HARD = 3;
+    public static final int EXPERT = 4;
+    public static final int MASTER = 5;
 
     private static final int[] allLevels = new int[] { EASY, NORMAL, HARD, EXPERT, MASTER};
 
