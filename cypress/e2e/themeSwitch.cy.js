e7f7f268df6039624a0bfdf3967812e31cfc7efa
RemakingEden <joss.sparkes@gmail.com>
2023-05-06 15:06:11 +0100

    Added pages to the code to make the tests easier to read, also fixed some data-cy names

--- cypress/e2e/themeSwitch.cy.js
+++ cypress/e2e/themeSwitch.cy.js
@@ -1,5 +1,3 @@
-/// <reference types="cypress" />
-
 describe('Theme switching', () => {
   beforeEach(() => {
   })
@@ -7,14 +5,14 @@ describe('Theme switching', () => {
   it("A user can switch the theme to dark on each page", () => {
     cy.wrap(["", "challenge/0", "stats", "about"]).each((endpoint) => {
       cy.visit(`localhost:8080/${endpoint}`)
-      cy.get('[data-cy=dark-mode-radio').click()
+      cy.dataCy('dark-mode-radio').click()
       cy.get('.dark-mode').should('exist')
     })
   })
 
   it("Dark mode persists on each page", () => {
     cy.visit('localhost:8080')
-    cy.get('[data-cy=dark-mode-radio').click()
+    cy.dataCy('dark-mode-radio').click()
     cy.wrap(["", "challenge/0", "stats", "about"]).each((endpoint) => {
       cy.visit(`localhost:8080/${endpoint}`)
       cy.get('.dark-mode').should('exist')
@@ -24,14 +22,14 @@ describe('Theme switching', () => {
   it("A user can switch the theme to light on each page", () => {
     cy.wrap(["", "challenge/0", "stats", "about"]).each((endpoint) => {
       cy.visit(`localhost:8080/${endpoint}`)
-      cy.get('[data-cy=light-mode-radio').click() 
+      cy.dataCy('light-mode-radio').click() 
       cy.get('.dark-mode').should('not.exist')
     })
   })
 
   it("Light mode persists on each page", () => {
     cy.visit('localhost:8080')
-    cy.get('[data-cy=light-mode-radio').click()
+    cy.dataCy('light-mode-radio').click()
     cy.wrap(["", "challenge/0", "stats", "about"]).each((endpoint) => {
       cy.visit(`localhost:8080/${endpoint}`)
       cy.get('.dark-mode').should('not.exist')
@@ -41,8 +39,8 @@ describe('Theme switching', () => {
   it("A user can switch theme to dark and back to light on each page", () => {
     cy.wrap(["", "challenge/0", "stats", "about"]).each((endpoint) => {
       cy.visit(`localhost:8080/${endpoint}`)
-      cy.get('[data-cy=dark-mode-radio').click()
-      cy.get('[data-cy=light-mode-radio').click()
+      cy.dataCy('dark-mode-radio').click()
+      cy.dataCy('light-mode-radio').click()
       cy.get('.dark-mode').should('not.exist')
     })
   })
