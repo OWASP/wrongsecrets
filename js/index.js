c2f901487a38ee71efac0cd83ce79fb436dd8cf3
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2023-05-28 08:45:49 +0200

    Fix for challenge13 to not have base64 as input

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
-/* eslint-disable no-unused-vars */
-function secret () {
-  const password = 'jLJIQZM=' + 9 + 'aG7l' + 6 + 'LAQ=' + 2 + 'UdiM' + 7
-  return password
-}
+function secret() { 
+ var password = "tJbQjCM=" + 9 + "SnCq" + 6 + "LBU=" + 2 + "h5GD" + 7;
+ return password;
+ }
