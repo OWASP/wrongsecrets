926ce70c218a7f58939d690bef5b736b6a484acf
RemakingEden <joss.sparkes@gmail.com>
2023-07-17 19:39:42 +0100

    Feature(OWASP#614): Fixed some issues with the merge

--- src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge35.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge35.java
@@ -1,12 +1,6 @@
 package org.owasp.wrongsecrets.challenges.docker;
 
-<<<<<<< HEAD
-import com.google.common.base.Strings;
 import java.util.List;
-import lombok.extern.slf4j.Slf4j;
-=======
-import java.util.List;
->>>>>>> cefa8809 ( Feature(#614): Added test files)
 import org.owasp.wrongsecrets.RuntimeEnvironment;
 import org.owasp.wrongsecrets.ScoreCard;
 import org.owasp.wrongsecrets.challenges.Challenge;
@@ -14,31 +8,15 @@ import org.owasp.wrongsecrets.challenges.ChallengeTechnology;
 import org.owasp.wrongsecrets.challenges.Difficulty;
 import org.owasp.wrongsecrets.challenges.Spoiler;
 import org.springframework.core.annotation.Order;
-<<<<<<< HEAD
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
-import org.springframework.stereotype.Component;
-
-/**
- * This is a challenge based on the belief that a static string processed by Pbkdf2 can lead to a
- * seecure setup. <a href="https://www.dcode.fr/pbkdf2-hash">online generator</a>
- */
-@Slf4j
-=======
 import org.springframework.stereotype.Component;
 
 /**
  * This is a challenge based on leaking secrets with the misuse of Git notes
  */
->>>>>>> cefa8809 ( Feature(#614): Added test files)
 @Component
 @Order(34)
 public class Challenge34 extends Challenge {
 
-<<<<<<< HEAD
-  private String key;
-
-=======
->>>>>>> cefa8809 ( Feature(#614): Added test files)
   public Challenge34(ScoreCard scoreCard) {
     super(scoreCard);
   }
@@ -50,34 +28,17 @@ public class Challenge34 extends Challenge {
 
   @Override
   public Spoiler spoiler() {
-<<<<<<< HEAD
-    return new Spoiler(getKey());
-=======
     return new Spoiler(getSolution());
->>>>>>> cefa8809 ( Feature(#614): Added test files)
   }
 
   @Override
   public boolean answerCorrect(String answer) {
-<<<<<<< HEAD
-    return getKey().equals(answer);
-=======
     return getSolution().equals(answer);
->>>>>>> cefa8809 ( Feature(#614): Added test files)
   }
 
   /** {@inheritDoc} */
   @Override
   public int difficulty() {
-<<<<<<< HEAD
-    return Difficulty.NORMAL;
-  }
-
-  /** {@inheritDoc} This is a crypto (hashing) type of challenge */
-  @Override
-  public String getTech() {
-    return ChallengeTechnology.Tech.CRYPTOGRAPHY.id;
-=======
     return Difficulty.EASY;
   }
 
@@ -85,7 +46,6 @@ public class Challenge34 extends Challenge {
   @Override
   public String getTech() {
     return ChallengeTechnology.Tech.GIT.id;
->>>>>>> cefa8809 ( Feature(#614): Added test files)
   }
 
   @Override
@@ -98,30 +58,6 @@ public class Challenge34 extends Challenge {
     return List.of(RuntimeEnvironment.Environment.DOCKER);
   }
 
-<<<<<<< HEAD
-  private String getKey() {
-    if (Strings.isNullOrEmpty(key)) {
-      key = generateKey();
-    }
-    log.info(
-        "The key for challenge34 has been initialized, now you can use it for encryption when"
-            + " needed.");
-    return key;
-  }
-
-  private String generateKey() {
-    String encryptedKey = "123%78___+WEssweLKWEJROUVHLAMW,NERO";
-    // note the static salt in use to get to the same key. otherwise the key is not reusable.
-    Pbkdf2PasswordEncoder encoder =
-        new Pbkdf2PasswordEncoder(
-            "secret_salt",
-            0,
-            100000,
-            Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
-    encoder.setEncodeHashAsBase64(true);
-
-    return encoder.encode(encryptedKey);
-=======
   private String getSolution() {
     return unobfuscate("UOZFGZTLOLLXHTKEGGS");
   }
@@ -140,6 +76,5 @@ public class Challenge34 extends Challenge {
       }
     }
     return plainText.toString();
->>>>>>> cefa8809 ( Feature(#614): Added test files)
   }
 }
