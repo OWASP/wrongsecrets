1dcbbdb27f0902e2ae6208aff029316188df6385
Nanne Baars <nanneb@gmail.com>
2023-08-29 14:24:02 +0200

    fix: configure basic-auth username/password

--- src/main/java/org/owasp/wrongsecrets/challenges/docker/authchallenge/AuthenticationChallengeSecurityConfig.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/authchallenge/AuthenticationChallengeSecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 /* Ensures the need for basic auth for the endpoint having the actual Secret */
@@ -17,6 +22,13 @@ public class AuthenticationChallengeSecurityConfig {
     public SecurityFilterChain configureBasicAuthForChallenge(HttpSecurity http) throws Exception {
         return http
             .authorizeRequests().requestMatchers("/authenticated/**").authenticated().and().httpBasic(Customizer.withDefaults()).build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.builder().username("admin").password("{noop}admin").roles("ADMIN")
+            .build();
+        return new InMemoryUserDetailsManager(admin);
     }
 
 
