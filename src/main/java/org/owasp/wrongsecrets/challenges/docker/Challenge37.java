30dd292919c7296376850b8108a94680e2c4df19
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2023-09-30 00:58:00 +0200

    retry fix ZAP auth issue

--- src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge37.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge37.java
@@ -27,7 +27,7 @@ import org.springframework.stereotype.Component;
 public class Challenge37 extends Challenge {
 
   private String secret;
-  private static final String password = "WWpOQ2JHSnBRbnBhV0U1b1lsZFZTd289Cg==";
+  private static final String password = "YjNCbGJpQnpaWE5oYldVPQo=";
 
   public Challenge37(ScoreCard scoreCard) {
     super(scoreCard);
