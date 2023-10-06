aa1db619140c0326d7186c5b8a4697ca1cd701d0
Adarsh A <adarsh.a@thoughtworks.com>
2023-10-06 06:53:23 +0530

    feat: add challenge 38
    Signed-off-by: Adarsh A <adarsh.a@thoughtworks.com>

---
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge39.java
@@ -0,0 +1,100 @@
+package org.owasp.wrongsecrets.challenges.docker;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.List;
+import java.util.Objects;
+
+import org.owasp.wrongsecrets.RuntimeEnvironment;
+import org.owasp.wrongsecrets.ScoreCard;
+import org.owasp.wrongsecrets.challenges.Challenge;
+import org.owasp.wrongsecrets.challenges.ChallengeTechnology;
+import org.owasp.wrongsecrets.challenges.Difficulty;
+import org.owasp.wrongsecrets.challenges.Spoiler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+/** This is a challenge based on leaking secrets with the misuse of Git notes */
+@Slf4j
+@Component
+@Order(39)
+public class Challenge39 extends Challenge {
+
+  private Resource resource;
+
+  public Challenge39(
+      ScoreCard scoreCard, @Value("classpath:executables/secrchallenge.md") Resource resource) {
+    super(scoreCard);
+    this.resource = resource;
+  }
+
+  @Override
+  public boolean canRunInCTFMode() {
+    return true;
+  }
+
+  @Override
+  public Spoiler spoiler() {
+    return new Spoiler(getSolution());
+  }
+
+  @Override
+  public boolean answerCorrect(String answer) {
+    return getSolution().equals(answer);
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public int difficulty() {
+    return Difficulty.EASY;
+  }
+
+  /** {@inheritDoc} Cryptography based. */
+  @Override
+  public String getTech() {
+    return ChallengeTechnology.Tech.CRYPTOGRAPHY.id;
+  }
+
+  @Override
+  public boolean isLimitedWhenOnlineHosted() {
+    return false;
+  }
+
+  @Override
+  public List<RuntimeEnvironment.Environment> supportedRuntimeEnvironments() {
+    return List.of(RuntimeEnvironment.Environment.DOCKER);
+  }
+
+  private String getSolution() {
+    try {
+      String encryptedText = resource.getContentAsString(Charset.defaultCharset());
+      byte[] decodedEncryptedText = Base64.getDecoder().decode(encryptedText.trim());
+      byte[] decodedKey =
+          Objects.requireNonNull(resource.getFilename()).getBytes(StandardCharsets.UTF_8);
+
+      // Create a SecretKey from the plaintext key
+      SecretKey secretKey = new SecretKeySpec(decodedKey, "AES");
+
+      // Initialize the Cipher for decryption
+      Cipher cipher = Cipher.getInstance("AES");
+      cipher.init(Cipher.DECRYPT_MODE, secretKey);
+
+      // Decrypt the data
+      byte[] decryptedData = cipher.doFinal(decodedEncryptedText);
+
+      // Convert the decrypted bytes to a String
+      return new String(decryptedData, StandardCharsets.UTF_8);
+    } catch (Exception e) {
+      log.warn("there was an exception with decrypting content in challenge39", e);
+      return "error_decryption";
+    }
+  }
+}
