fe35f81d0a3cd24d20c271358e82da2f5b9d5597
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2023-12-17 08:57:02 +0100

    building out basics for challenge 44

--- src/main/java/org/owasp/wrongsecrets/challenges/kubernetes/MetaDataChallenge.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/kubernetes/MetaDataChallenge.java
@@ -1,19 +1,44 @@
 package org.owasp.wrongsecrets.challenges.kubernetes;
 
+import com.google.common.base.Strings;
 import java.util.Objects;
+import lombok.extern.slf4j.Slf4j;
 import org.owasp.wrongsecrets.challenges.FixedAnswerChallenge;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.vault.client.VaultEndpoint;
 import org.springframework.vault.core.*;
 import org.springframework.vault.support.Versioned;
 
+/**
+ * This challenge is about having a metadata of secrets stored in a misconfigured Hashicorp Vault.
+ */
+@Component
+@Slf4j
 public class MetaDataChallenge extends FixedAnswerChallenge {
 
+  private final String vaultPasswordString;
+
+  public MetaDataChallenge(@Value("${vaultpassword}") String vaultPasswordString) {
+    this.vaultPasswordString = vaultPasswordString;
+  }
+
   public String getAnswer() {
-    VaultOperations operations = new VaultTemplate(new VaultEndpoint());
-    VaultVersionedKeyValueOperations versionedOperations =
-        operations.opsForVersionedKeyValue("wrongsecret");
-    Versioned<String> versioned = versionedOperations.get("metadatafun", String.class);
-    assert versioned != null;
-    return Objects.requireNonNull(versioned.getMetadata()).getCustomMetadata().get("secret");
+    try {
+      VaultOperations operations = new VaultTemplate(new VaultEndpoint());
+      VaultVersionedKeyValueOperations versionedOperations =
+          operations.opsForVersionedKeyValue("wrongsecret");
+      Versioned<String> versioned = versionedOperations.get("metadatafun", String.class);
+      assert versioned != null;
+      String metadata =
+          Objects.requireNonNull(versioned.getMetadata()).getCustomMetadata().get("secret");
+      if (Strings.isNullOrEmpty(metadata)) {
+        return vaultPasswordString;
+      }
+      return metadata;
+    } catch (Exception e) {
+      log.warn("Exception during exection of challenge44", e);
+      return vaultPasswordString;
+    }
   }
 }
--- src/test/java/org/owasp/wrongsecrets/challenges/kubernetes/Challenge44Test.java
+++ src/test/java/org/owasp/wrongsecrets/challenges/kubernetes/Challenge44Test.java
@@ -1,3 +1,3 @@
 package org.owasp.wrongsecrets.challenges.kubernetes;
 
-public class Challenge43Test {}
+public class Challenge44Test {}
