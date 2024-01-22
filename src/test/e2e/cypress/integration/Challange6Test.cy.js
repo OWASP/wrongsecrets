81ffec5a74545387c5293a6b713ff3b95f1552f9
Shubham-Patel07 <shubham.patel.btech2020@sitpune.edu.in>
2024-01-22 20:20:36 +0530

    Fix: cypress scripts for challange-6

--- src/test/e2e/cypress/integration/Challange6Test.cy.js
+++ src/test/e2e/cypress/integration/Challange6Test.cy.js
@@ -1,15 +1,15 @@
-describe('Challenge5 Tests', () => {
+describe('Challenge6 Tests', () => {
     it('Submitting a Correct Answer', () => {
-        cy.visit('/challenge/challenge-5');
+        cy.visit('/challenge/challenge-6');
 
-        cy.get('#answerfield').type('thisIsK8SConfigMap');
+        cy.get('#answerfield').type('this is apassword');
         cy.get('[data-cy=submit-textbox-btn]').click();
 
         cy.get('[data-cy=success-alert]').should('contain', 'correct');
     });
 
     it('Submitting an Incorrect Answer', () => {
-        cy.visit('/challenge/challenge-5');
+        cy.visit('/challenge/challenge-6');
 
         cy.get('#answerfield').type('wrong_answer');
         cy.get('[data-cy=submit-textbox-btn]').click();
