4a2fd2e27f0458b626e05160b16ea65415a9f3c2
Shubham-Patel07 <shubham.patel.btech2020@sitpune.edu.in>
2024-02-01 23:16:49 +0530

    Add: workflow update

--- src/test/K8s-tests/cypress.config.js
+++ src/test/K8s-tests/cypress.config.js
@@ -1,8 +1,13 @@
-module.exports = {
-  e2e: {
-      baseUrl: 'http://localhost:8080/',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+    video: false,
+    e2e: {
+        baseUrl: 'http://localhost:8080/',
+        specPattern: 'cypress/e2e/*.cy.js',
+        supportFile: false,
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
     },
-  },
-};
+});
