7405b9975c498eb6b45f904e98a22c014b7f67ea
Shubham-Patel07 <shubham.patel.btech2020@sitpune.edu.in>
2024-02-04 23:52:54 +0530

    Add: reverted unwanted changes

--- src/main/resources/static/theme-toggle.js
+++ src/main/resources/static/theme-toggle.js
@@ -1,38 +1,34 @@
 (function () {
-  const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
-
-  function updateToggle (darkMode) {
-    const checkbox = document.querySelector(".theme-toggle input[type='checkbox']")
-    if (checkbox) {
-      checkbox.checked = darkMode
-    }
-
-    document.body.classList.toggle('dark-mode', darkMode)
-    localStorage.setItem('darkMode', darkMode)
-    localStorage.setItem('darkmode-pref-set', 'true')
-  }
-
-  // Listen for system theme changes
-  darkModeMediaQuery.addEventListener('change', (e) => {
-    updateToggle(e.matches)
-  })
-
-  // Set up event listener and initial theme on page load
-  window.addEventListener('load', function () {
-    const checkbox = document.querySelector(".theme-toggle input[type='checkbox']")
-    if (checkbox) {
-      checkbox.addEventListener('change', function () {
-        updateToggle(checkbox.checked)
-      })
-
-      // Initialize theme based on user preference or system preference
-      let initialTheme
-      if (localStorage.getItem('darkmode-pref-set') === 'true') {
+    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+    let initialTheme
+    if (localStorage.getItem('darkmode-pref-set') === 'true') {
         initialTheme = localStorage.getItem('darkMode') === 'true'
-      } else {
+    } else {
         initialTheme = darkModeMediaQuery.matches
-      }
-      updateToggle(initialTheme)
     }
-  })
+
+    function updateToggle (darkMode) {
+        document.querySelector(".theme-toggle input[type=radio][value='dark']").checked = darkMode
+        document.querySelector(".theme-toggle input[type=radio][value='light']").checked = !darkMode
+
+        document.body.classList.toggle('dark-mode', darkMode)
+        localStorage.setItem('darkMode', darkMode)
+        localStorage.setItem('darkmode-pref-set', 'true')
+    }
+
+    darkModeMediaQuery.addEventListener('change', (e) => {
+        const darkModeOn = e.matches
+        updateToggle(darkModeOn)
+    })
+
+    window.addEventListener('load', function () {
+        const radios = document.querySelectorAll('.theme-toggle input[type=radio]')
+        radios.forEach((radio) => {
+            radio.addEventListener('change', function (e) {
+                updateToggle(e.target.value === 'dark')
+            })
+        })
+
+        updateToggle(initialTheme)
+    })
 })()
