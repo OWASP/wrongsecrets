075a6896c6a2d4b30ab35cdd965697ea9e18d151
Shubham-Patel07 <shubham.patel.btech2020@sitpune.edu.in>
2024-02-11 20:21:48 +0530

    Add: Updated tests for the modified button

--- src/test/e2e/cypress/integration/themeSwitch.cy.js
+++ src/test/e2e/cypress/integration/themeSwitch.cy.js
@@ -1,13 +1,18 @@
 import ThemeSwitchPage from '../pages/themeSwitchPage'
 
 describe('Theme Switching Tests', () => {
-  it('A user can switch the theme to dark on each page', () => {
-    cy.wrap(['', 'challenge/challenge-0', 'stats', 'about']).each((endpoint) => {
-      cy.visit(`/${endpoint}`)
-      cy.dataCy(ThemeSwitchPage.DARK_MODE_RADIO).click()
-      cy.get(ThemeSwitchPage.DARK_MODE).should('exist')
-    })
-  })
+    it('A user can switch the theme to dark on each page', () => {
+        cy.wrap(['', 'challenge/challenge-0', 'stats', 'about']).each((endpoint) => {
+            cy.visit(`/${endpoint}`);
+            cy.get('body').then(($body) => {
+                if (!$body.hasClass('dark-mode')) { // Adjust this condition to check for light mode
+                    cy.dataCy(ThemeSwitchPage.DARK_MODE_RADIO).click();
+                }
+            });
+            cy.get(ThemeSwitchPage.DARK_MODE).should('exist');
+            cy.dataCy(ThemeSwitchPage.DARK_MODE_RADIO).click();
+        });
+    });
 
   it('Dark mode persists on each page', () => {
     cy.visit('/')
