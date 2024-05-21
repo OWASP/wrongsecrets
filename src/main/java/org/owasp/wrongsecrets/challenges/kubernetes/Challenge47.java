ee5c57fe4485b591dc713ca942246b52286ad934
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2024-05-21 12:00:19 +0200

    added missing component annotation and test coverage

--- src/main/java/org/owasp/wrongsecrets/challenges/kubernetes/Challenge47.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/kubernetes/Challenge47.java
@@ -2,11 +2,14 @@ package org.owasp.wrongsecrets.challenges.kubernetes;
 
 import org.owasp.wrongsecrets.challenges.FixedAnswerChallenge;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Challenge47 extends FixedAnswerChallenge {
 
   private final String secret;
 
+  /** This challenge is about having a secrets injected via Vault template. */
   public Challenge47(@Value("${challenge47secret}") String secret) {
     this.secret = secret;
   }
