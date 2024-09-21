d68e3f61116fd96fce3725662402d3845385ab53
Shubham-Patel07 <shubham.patel.btech2020@sitpune.edu.in>
2024-09-22 00:05:49 +0530

    Add: created java class Challenge49

---
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge49.java
@@ -0,0 +1,20 @@
+package org.owasp.wrongsecrets.challenges.docker;
+
+import org.owasp.wrongsecrets.challenges.FixedAnswerChallenge;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Challenge49 extends FixedAnswerChallenge {
+
+    private final String dockerSecret;
+
+    public Challenge49(@Value("DOCKER_SECRET_CHALLENGE49") String dockerSecret) {
+        this.dockerSecret = dockerSecret;
+    }
+
+    @Override
+    public String getAnswer() {
+        return this.dockerSecret;
+    }
+}
