e362773a819374c3c4bbb326df22e17bb639ab7c
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2024-09-30 14:25:53 +0200

    LFS version of challnege for #1243

---
+++ src/test/java/org/owasp/wrongsecrets/challenges/docker/Challenge50Test.java
@@ -0,0 +1,18 @@
+package org.owasp.wrongsecrets.challenges.docker;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.owasp.wrongsecrets.challenges.docker.binaryexecution.BinaryExecutionHelper.ERROR_EXECUTION;
+
+import org.junit.jupiter.api.Test;
+import org.owasp.wrongsecrets.challenges.Spoiler;
+
+class Challenge50Test {
+
+  @Test
+  void spoilerShouldNotCrash() {
+    var challenge = new Challenge50();
+
+    assertThat(challenge.spoiler()).isNotEqualTo(new Spoiler(ERROR_EXECUTION));
+    assertThat(challenge.answerCorrect(challenge.spoiler().solution())).isTrue();
+  }
+}
