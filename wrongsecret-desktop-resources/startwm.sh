ec41111b0d0131c980ffc0ec6e5ba8b1b69032ea
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2024-10-09 00:06:57 +0200

    .net container support

--- wrongsecret-desktop-resources/startwm.sh
+++ wrongsecret-desktop-resources/startwm.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-export DOTNET_ROOT=$HOME/.dotnet
-export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
+export DOTNET_ROOT=/etc/dotnet
+export DOTNET_INSTALL_DIR="/etc/dotnet"
+export PATH="$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools"
+export PATH="$PATH:/config/.dotnet/tools"
 /startpulse.sh &
 ln -s -r /var/tmp/wrongsecrets /config/Desktop/wrongsecrets
 ln -s /var/tmp/wrongsecrets/welcome.md /config/Desktop/welcome.md
