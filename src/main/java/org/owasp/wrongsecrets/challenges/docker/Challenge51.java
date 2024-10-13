2de13204723855179d01ae800a3964a6ec61ec09
Shubham-Patel07 <shubham.patel.btech2020@sitpune.edu.in>
2024-10-13 20:34:42 +0530

    Add: added challenge51.java

---
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge51.java
@@ -0,0 +1,17 @@
+package org.owasp.wrongsecrets.challenges.docker;
+
+import org.owasp.wrongsecrets.challenges.FixedAnswerChallenge;
+import org.springframework.beans.factory.annotation.Value;
+
+public class Challenge51 extends FixedAnswerChallenge {
+    private final String dockerSecret;
+
+    public Challenge51(@Value("${DOCKER_SECRET_CHALLENGE51}") String dockerSecret) {
+        this.dockerSecret = dockerSecret;
+    }
+
+    @Override
+    public String getAnswer() {
+        return this.dockerSecret;
+    }
+}
