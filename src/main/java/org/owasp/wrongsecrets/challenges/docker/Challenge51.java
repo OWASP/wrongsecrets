8805e2c26a933b2be4df4362a41af507fee389e5
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2024-10-23 10:57:04 +0200

    add missing annotations

--- src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge51.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge51.java
@@ -1,8 +1,13 @@
 package org.owasp.wrongsecrets.challenges.docker;
 
+import lombok.extern.slf4j.Slf4j;
 import org.owasp.wrongsecrets.challenges.FixedAnswerChallenge;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+/** challenge about docker compose secrets */
+@Slf4j
+@Component
 public class Challenge51 extends FixedAnswerChallenge {
   private final String dockerSecret;
 
