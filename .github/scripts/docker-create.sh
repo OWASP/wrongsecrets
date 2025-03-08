28cb5494cddc550a41d5fef8705693711cb91e93
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2025-03-08 06:32:10 +0100

    now make hte container arm/x84 compatible

--- .github/scripts/docker-create.sh
+++ .github/scripts/docker-create.sh
@@ -372,6 +372,7 @@ create_containers() {
         docker buildx build --platform linux/amd64,linux/arm64 -t jeroenwillemsen/wrongsecrets-desktop-k8s:$tag -f Dockerfile_webdesktopk8s --secret id=mysecret,env=SECRET_VALUE --push .
         docker buildx build --platform linux/amd64,linux/arm64 -t jeroenwillemsen/wrongsecrets-desktop-k8s:latest -f Dockerfile_webdesktopk8s --secret id=mysecret,env=SECRET_VALUE --push .
         cd k8s/challenge52
+        #todo: cp the challenge52 files to the right location instead of having 2 copies
         docker buildx build --platform linux/amd64,linux/arm64 -t jeroenwillemsen/wrongsecrets-challenge52:$tag -f Dockerfile --push .
         docker buildx build --platform linux/amd64,linux/arm64 -t jeroenwillemsen/wrongsecrets-challenge52-debug:$tag -f Dockerfile.debug --push .
         cd ../..
