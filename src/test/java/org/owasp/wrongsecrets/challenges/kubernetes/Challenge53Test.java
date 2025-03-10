72e3feb1497367d3dfc5924139aa74bea43fa0b9
Pastekitoo <tanguy.cavelier@univ-rouen.fr>
2025-03-10 13:05:23 +0100

    Update Challenge53Test.java

--- src/test/java/org/owasp/wrongsecrets/challenges/kubernetes/Challenge53Test.java
+++ src/test/java/org/owasp/wrongsecrets/challenges/kubernetes/Challenge53Test.java
@@ -4,18 +4,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
-public class Challenge52Test {
+public class Challenge53Test {
 
   @Test
   void spoilerShouldGiveAnswer() {
-    var challenge = new Challenge52("answer");
+    var challenge = new Challenge53();
     assertThat(challenge.spoiler().solution()).isEqualTo("answer");
     assertThat(challenge.answerCorrect(challenge.spoiler().solution())).isTrue();
   }
 
   @Test
   void incorrectAnswerShouldNotSolveChallenge() {
-    var challenge = new Challenge52("answer");
-    assertThat(challenge.answerCorrect("wrong answer")).isFalse();
+    var challenge = new Challenge53();
+    assertThat(challenge.answerCorrect("puthrightanswerhereunfortunately")).isFalse();
   }
 }
