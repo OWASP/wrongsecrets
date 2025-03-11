e8a8bdd8eba0d55d243a3f700ce9ffe92b596f28
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2025-03-11 09:04:03 +0100

    update gcp readme and script

--- gcp/k8s-vault-gcp-start.sh
+++ gcp/k8s-vault-gcp-start.sh
@@ -5,7 +5,7 @@
 
 source ../scripts/check-available-commands.sh
 
-checkCommandsAvailable helm jq vault sed grep cat gcloud envsubst
+checkCommandsAvailable cat envsubst gcloud grep helm jq kubectl sed terraform vault
 
 echo "This is a script to bootstrap the configuration. You need to have installed: helm, kubectl, jq, vault, grep, cat, sed, envsubst, and google cloud cli, and is only tested on mac, Debian and Ubuntu"
 echo "This script is based on the steps defined in https://learn.hashicorp.com/tutorials/vault/kubernetes-minikube. Vault is awesome!"
