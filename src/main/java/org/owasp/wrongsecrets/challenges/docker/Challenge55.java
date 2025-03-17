9a764c980ba300733ca24ad17c120bcd3a458a32
Pastekitoo <tanguy.cavelier@univ-rouen.fr>
2025-03-17 15:37:12 +0100

    Update Challenge55.java

--- src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge55.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge55.java
@@ -1,5 +1,6 @@
 package org.owasp.wrongsecrets.challenges;
 
+import org.owasp.wrongsecrets.challenges.FixedAnswerChallenge;
 import org.springframework.stereotype.Component;
 
 /** Challenge with a secret in .ssh */
