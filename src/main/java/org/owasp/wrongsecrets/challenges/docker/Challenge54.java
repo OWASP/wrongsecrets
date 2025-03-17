0f0983b2c9ba4f5fcea5479f0d1d079c8351b40b
Pastekitoo <tanguy.cavelier@univ-rouen.fr>
2025-03-17 15:34:31 +0100

    Update Challenge54.java

--- src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge54.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge54.java
@@ -1,5 +1,6 @@
 package org.owasp.wrongsecrets.challenges.docker;
 
+import org.owasp.wrongsecrets.challenges.FixedAnswerChallenge;
 import org.springframework.stereotype.Component;
 
 /** Challenge with a secret in .gitignore */
