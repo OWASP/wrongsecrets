f7700a2786599ee73620b4b3a9414af50efe0929
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2026-09-06 07:50:15 +0200

    Moved to aes gcm

--- src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge68.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/Challenge68.java
@@ -5,7 +5,7 @@ import static org.owasp.wrongsecrets.Challenges.ErrorResponses.DECRYPTION_ERROR;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import javax.crypto.Cipher;
-import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import lombok.extern.slf4j.Slf4j;
 import org.owasp.wrongsecrets.challenges.FixedAnswerChallenge;
@@ -16,18 +16,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class Challenge68 extends FixedAnswerChallenge {
 
-  private static final String CIPHERTEXT = "dagGnT4fdF7/z010sAHhhp1t6w2NsUQkHy9+Fx1LCG0=";
+  private static final String CIPHERTEXT =
+      "etl6oWVrKiBvZWM6eLdvzdtkKz+AHrsJltbkl0JQ8SY1qZpQD+WqrOG1ckJfaDo=";
 
   @Override
   public String getAnswer() {
     try {
       byte[] keyBytes = "SuperSecretKey12".getBytes(StandardCharsets.UTF_8);
-      byte[] ivBytes = "InitVector123456".getBytes(StandardCharsets.UTF_8);
+      byte[] ivBytes = "InitVector12".getBytes(StandardCharsets.UTF_8);
       byte[] cipherBytes = Base64.getDecoder().decode(CIPHERTEXT);
       SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "AES");
-      IvParameterSpec ivSpec = new IvParameterSpec(ivBytes);
-      Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-      cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
+      GCMParameterSpec gcmSpec = new GCMParameterSpec(128, ivBytes);
+      Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+      cipher.init(Cipher.DECRYPT_MODE, keySpec, gcmSpec);
       byte[] decrypted = cipher.doFinal(cipherBytes);
       return new String(decrypted, StandardCharsets.UTF_8).trim();
     } catch (Exception e) {
