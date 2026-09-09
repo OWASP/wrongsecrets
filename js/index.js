fc7d6c81ca723f1a6e47db0c24cdc0620c4cd5dc
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2026-09-09 12:15:18 +0200

    Update POM file with new version: 1.14.0RC4

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-
- function secret() {
- var password = "VeZGFY4=" + 9 + "xyOd" + 6 + "VcA=" + 2 + "LCiO" + 7;
+// eslint-disable-next-line no-unused-vars
+ function secret() { 
+ var password = "ZFxrE9Y=" + 9 + "kvuo" + 6 + "yp4=" + 2 + "lrgF" + 7;
  return password;
  }
