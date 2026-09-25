88105be9e2fbc5f4121160c68632adc9011d8266
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2026-09-25 06:32:24 +0200

    updated k8s setup

--- k8s-vault-minikube-start.sh
+++ k8s-vault-minikube-start.sh
@@ -175,11 +175,11 @@ kubectl create serviceaccount vault
  kubectl apply -f k8s/challenge74/wrongsecrets-llama-secret.yaml
  kubectl apply -f k8s/challenge74/wrongsecrets-llama-service.yaml
  kubectl apply -f k8s/challenge74/wrongsecrets-llama-deployment.yaml
- while [[ $(kubectl get pods -l app=wrongsecrets-llama -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do
+ while [[ $(kubectl get pods -l app=challenge74-llama -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do
    echo "waiting for wrongsecrets-llama" && sleep 2
  done
- kubectl get pods -l app=wrongsecrets-llama
- kubectl logs deployment/wrongsecrets-llama
+ kubectl get pods -l app=challenge74-llama
+ kubectl logs deployment/challenge74-llama
 
 
 echo "Deploy secret challenge app"
